83d0a74a4181fea70092558248e567464d45a377
burak-senel <buurak.senel@gmail.com>
2024-03-16 02:05:12 +0300

    sdafsdf

--- checkstock.js
+++ checkstock.js
@@ -19,7 +19,7 @@ function checkStock() {
     https.get(url, (response) => {
         let data = '';
 
-        response.on('data', (chunk) => {
+        response.on('data', (chunk) => {sdfsdf
             data += chunk;
         });
 
